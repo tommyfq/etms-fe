da6727707a581145d49281bed813c7f89202e727
tommyfq <t.quiko@bumi.ai>
2024-12-23 21:01:16 +0700

    :lipstick: change UI login

--- src/app/modules/auth/AuthLayout.tsx
+++ src/app/modules/auth/AuthLayout.tsx
@@ -17,9 +17,31 @@ const AuthLayout = () => {
   }, [])
 
   return (
-    <div className='d-flex flex-column flex-lg-row flex-column-fluid h-100'>
+    <div className='d-flex flex-column flex-lg-row flex-column-fluid h-100' >
+      <style>
+        {
+          `
+          .overlay-image-bg {
+            position: fixed; /* Ensures the image stays in the same place even when scrolling */
+            top: 0;
+            left: 0;
+            width: 50vw; /* Full width of the viewport */
+            height: 100vh; /* Full height of the viewport */
+            z-index: 9999; /* Puts the image on top of everything */
+            object-fit: cover; /* Ensures the image scales nicely to cover the area */
+            pointer-events: none; /* Allows interactions with the content underneath */
+          }
+          `
+        }
+      </style>
+      
       {/* begin::Body */}
       <div className='d-flex flex-column flex-lg-row-fluid w-lg-50 p-10 order-2 order-lg-1'>
+        <img
+          src={toAbsoluteUrl('media/misc/auth-background2.png')}
+          alt="Overlay"
+          className="overlay-image-bg"
+        />
         {/* begin::Form */}
         <div className='d-flex flex-center flex-column flex-lg-row-fluid'>
           {/* begin::Wrapper */}
@@ -55,7 +77,7 @@ const AuthLayout = () => {
       {/* begin::Aside */}
       <div
         className='d-flex flex-lg-row-fluid w-lg-50 bgi-size-cover bgi-position-center order-1 order-lg-2'
-        style={{backgroundImage: `url(${toAbsoluteUrl('media/misc/auth-bg.png')})`}}
+        style={{backgroundImage: `url(${toAbsoluteUrl('media/misc/auth-content2.png')})`}}
       >
         {/* begin::Content */}
         <div className='d-flex flex-column flex-center py-15 px-5 px-md-15 w-100'>
@@ -67,17 +89,17 @@ const AuthLayout = () => {
           {/* end::Logo */}
 
           {/* begin::Image */}
-          <img
+          {/* <img
             className='mx-auto w-275px w-md-50 w-xl-500px mb-10 mb-lg-20'
             src={toAbsoluteUrl('media/misc/auth-content.png')}
             alt=''
-          />
+          /> */}
           {/* end::Image */}
 
           {/* begin::Title */}
-          <h1 className='text-white fs-2qx fw-bolder text-center mb-7'>
+          {/* <h1 className='text-white fs-2qx fw-bolder text-center mb-7'>
             Asset Service and Support Portal
-          </h1>
+          </h1> */}
           {/* end::Title */}
 
           {/* begin::Text */}
@@ -93,9 +115,9 @@ const AuthLayout = () => {
             </a>
             and their <br /> work following this is a transcript of the interview.
           </div> */}
-          <div className="text-white fs-base text-center">
+          {/* <div className="text-white fs-base text-center">
             This platform is designed to streamline your asset management process, <br/> offering easy access to helpdesk support, service requests, and maintenance tracking for all your assets. <br/> If you encounter any issues, please contact our office number.
-          </div>
+          </div> */}
           {/* end::Text */}
         </div>
         {/* end::Content */}
